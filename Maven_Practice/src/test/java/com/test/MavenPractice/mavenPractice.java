e82f7d7256980c5abff2badb558415665b25f1ab
MAQBOOL <MAQBOOL@DESKTOP-A5JFSKS>
2022-07-19 23:43:37 +0530

    hiiii

--- Maven_Practice/src/test/java/com/test/MavenPractice/mavenPractice.java
+++ Maven_Practice/src/test/java/com/test/MavenPractice/mavenPractice.java
@@ -5,18 +5,26 @@ import org.testng.annotations.Test;
 public class mavenPractice {
         @Test
         public void practiceTest1() {
-        	System.out.println("TYSS1");
+
+        	System.out.println("TYSS1");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+  
         	
         
-        	String password = System.getProperty("MISSIONIMPOSSIBLE"); 
-        	System.out.println(password);
+
         }
         @Test
         public void Test2() {
-        	System.out.println("hii");
+        	System.out.println("hiii");
         	System.out.println("bye");
+        	String url = System.getProperty("URL");
+        	String username = System.getProperty("USERNAME");
+        	String password = System.getProperty("PASSWORD");
+        	System.out.println(url);
+        	System.out.println(username);
+        	System.out.println(password);
         }
  
+
         
 
 }

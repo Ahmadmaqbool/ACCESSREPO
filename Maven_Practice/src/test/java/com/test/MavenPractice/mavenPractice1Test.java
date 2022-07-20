95965af415e878fa0e270768646a06167e995864
MAQBOOL <MAQBOOL@DESKTOP-A5JFSKS>
2022-07-20 15:41:06 +0530

    bro

--- Maven_Practice/src/test/java/com/test/MavenPractice/mavenPractice1Test.java
+++ Maven_Practice/src/test/java/com/test/MavenPractice/mavenPractice1Test.java
@@ -9,6 +9,9 @@ public class mavenPractice1Test {
        }
        @Test
        public void mavenPractice2Test() {
+    	   
+    	   String BROWSER = System.getProperty("browser");
+    	   String URL = System.getProperty("url");
     	   System.out.println("TestYantra1");
     	   //change
 
